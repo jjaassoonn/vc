f8dbf91139fa097fcf6c9b9e20d2ac392d86ce90
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-11-17 11:43:42 +0000

    use equivalence between affine schemes and rings op

--- src/points_of_scheme.lean
+++ src/points_of_scheme.lean
@@ -16,6 +16,7 @@ namespace algebraic_geometry
 
 open Scheme Top.presheaf opposite topological_space
 open category_theory
+open algebraic_geometry
 
 variables (X : Scheme.{u}) (R : Type u) [comm_ring R] [local_ring R]
 variable (f : Spec_obj (CommRing.of R) ⟶ X)
@@ -34,61 +35,27 @@ namespace Spec_local_ring_to_Scheme_auxs
 
 section affine_cases
 
-@[simps] def Spec_local_ring_to_Spec_equiv_ring_hom (S : Type u) [comm_ring S] :
+def Spec_local_ring_to_Spec_equiv_ring_hom (S : Type u) [comm_ring S] :
   ((Spec_obj $ CommRing.of R) ⟶ (Spec_obj $ CommRing.of S)) ≃ (CommRing.of S ⟶ CommRing.of R) :=
-{ to_fun := λ f, (structure_sheaf.global_sections_iso S).hom ≫ f.1.c.app (op ⊤) ≫ 
-    (structure_sheaf.global_sections_iso R).inv,
-  inv_fun := Spec_map,
-  left_inv := λ α, 
-  begin 
-    dsimp only,
-    rw [Spec_map_comp, Spec_map_comp, category.assoc],
-    haveI : is_iso (Spec_map (structure_sheaf.global_sections_iso R).hom),
-    { have h1 : Spec_map (structure_sheaf.global_sections_iso R).inv ≫ Spec_map (structure_sheaf.global_sections_iso R).hom = 𝟙 _,
-      { rw [←Spec_map_comp, iso.hom_inv_id, Spec_map_id], },
-      have h2 : Spec_map (structure_sheaf.global_sections_iso R).hom ≫ Spec_map (structure_sheaf.global_sections_iso R).inv = 𝟙 _,
-      { rw [←Spec_map_comp, iso.inv_hom_id, Spec_map_id], },
-      refine ⟨⟨_, h2, h1⟩⟩, },
-    have eq1 : Spec_map (structure_sheaf.global_sections_iso R).inv = inv (Spec_map (structure_sheaf.global_sections_iso R).hom),
-    { apply is_iso.eq_inv_of_hom_inv_id,
-      rw [←Spec_map_comp, iso.inv_hom_id, Spec_map_id], },
-    rw [eq1, is_iso.inv_comp_eq],
-    ext : 1,
-    have eq3 : ∀ ⦃A B C : Scheme.{u}⦄ (f : A ⟶ B) (g : B ⟶ C), (f ≫ g).1 = f.1 ≫ g.1 := λ _ _ _ _ _, rfl, 
-    rw [eq3],
-    ext : 1,
-    { have eq4 : ∀ ⦃A B C : Scheme.{u}⦄ (f : A ⟶ B) (g : B ⟶ C), (f ≫ g).1.c = g.1.c ≫ _ := λ _ _ _ _ _, rfl, 
-      rw [eq4],
-      ext U : 2,
-      erw [nat_trans.comp_app, nat_trans.comp_app],
-      rw [whisker_right_app, eq_to_hom_app],
-      change _ ≫ (Spec.structure_sheaf _).1.map _ = _,
-      rw [category.assoc],
-      erw nat_trans.naturality,
-          },
-  end,
-  right_inv := λ f, 
-  begin
-    dsimp,
-    rw [←category.assoc, is_iso.comp_inv_eq],
-    change structure_sheaf.to_open _ _ ≫ _ = _,
-    ext s x : 3,
-    rw [comp_apply, comp_apply],
-    simp only [←subtype.val_eq_coe],
-    rw structure_sheaf.to_open_apply,
-    dsimp only [Spec_map, Spec.to_LocallyRingedSpace_map, Spec.LocallyRingedSpace_map,
-      Spec.SheafedSpace_map, unop_op],
-    erw structure_sheaf.to_open_comp_comap_apply f ⊤ s,
-    refl,
-  end }
-
+(show (op ((Spec.structure_sheaf R).val.obj (op ⊤)) ⟶ (op $ CommRing.of S)) ≃ 
+    (Spec_obj (CommRing.of R) ⟶ Spec_obj (CommRing.of S)), 
+from 
+  AffineScheme.equiv_CommRing.to_adjunction.hom_equiv 
+  (AffineScheme.mk (Spec_obj $ CommRing.of R) 
+    (algebraic_geometry.Spec_is_affine (op $ CommRing.of R))) 
+  (op $ CommRing.of S)).symm.trans $ 
+(op_equiv _ _).trans $ 
+show (CommRing.of S ⟶ (Spec.structure_sheaf R).val.obj (op ⊤)) ≃ _, 
+from
+{ to_fun := λ f, f ≫ (structure_sheaf.global_sections_iso R).inv,
+  inv_fun := λ f, f ≫ (structure_sheaf.global_sections_iso R).hom,
+  left_inv := λ f, by simp_rw [category.assoc, iso.inv_hom_id, category.comp_id],
+  right_inv := λ f, by simp_rw [category.assoc, iso.hom_inv_id, category.comp_id] }
 variables [is_affine X]
 
 def Spec_local_ring_to_AffineScheme :
   ((Spec_obj $ CommRing.of R) ⟶ X) ≃ point_local_ring_hom_pair X R :=
-_
-
-
+sorry
 
 end affine_cases
 
@@ -112,24 +79,5 @@ def Spec_stalk_to_Scheme :
 { pt := (image_of_maximal_ideal f),
   ring_hom_ := ring_hom.id _,
   is_local_ring_hom := is_local_ring_hom_id _ }
-
-
-example : X.presheaf.stalk (image_of_maximal_ideal f) ⟶
-  (Spec.structure_sheaf R).presheaf.stalk ⟨_, (local_ring.maximal_ideal.is_maximal R).is_prime⟩ :=
-(stalk_functor _ (image_of_maximal_ideal f)).map f.1.c ≫ 
-stalk_pushforward _ _ _ _
-
-example : X.presheaf.stalk (image_of_maximal_ideal f) ⟶ CommRing.of R :=
-(stalk_functor _ (image_of_maximal_ideal f)).map f.1.c ≫ 
-stalk_pushforward _ _ _ _ ≫ (structure_sheaf.stalk_iso R ⟨_, (local_ring.maximal_ideal.is_maximal _).is_prime⟩).hom ≫ _
-
-
-
-
-include f
-example : true :=
-begin 
-have : X.presheaf.stalk (image_of_maximal_ideal f) ⟶ _ := (stalk_functor _ (image_of_maximal_ideal f)).map f.1.c,
-end
 
 end algebraic_geometry
