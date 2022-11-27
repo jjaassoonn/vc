8d79187a4243cc7293433e266bfb0f353f5b83e0
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-11-27 18:55:39 +0000

    add two algebra instance

--- src/random_lemmas.lean
+++ src/random_lemmas.lean
@@ -32,3 +32,41 @@ lemma ring_equiv_eq_iff_to_iso_eq {A B : Type*} [comm_ring A] [comm_ring B]
     from h ▸ rfl }
 
 end CommRing
+
+namespace algebra
+
+variables (R R' X X' : Type*) 
+variables [comm_ring R] [comm_ring R'] 
+variables [comm_ring X] [comm_ring X'] [algebra R X]
+
+@[simps] def of_equiv' (i : R ≃+* R') [algebra R X] : algebra R' X :=
+{ smul := λ r x, i.symm r • x,
+  commutes' := λ r x,
+  begin 
+    dsimp only,
+    rw mul_comm
+  end,
+  smul_def' := λ r x, 
+  begin 
+    dsimp only,
+    rw [smul_def],
+    refl,
+  end,
+  ..(algebra_map R X).comp i.symm.to_ring_hom }
+
+@[simps] def of_equiv (i : X ≃+* X') [algebra R X] : algebra R X' :=
+{ smul := λ r x, i $ r • i.symm x,
+  commutes' := λ r x, 
+  begin 
+    dsimp only,
+    rw mul_comm,
+  end,
+  smul_def' := λ r x, 
+  begin 
+    dsimp only [],
+    rw [smul_def, map_mul, ring_equiv.apply_symm_apply],
+    congr,
+  end,
+  ..i.to_ring_hom.comp (algebra_map R X) }
+
+end algebra
