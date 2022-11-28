26b6609e6011dd78e7595ee3e20b35d1d2f96ff7
Jujian Zhang <jujian.zhang1998@outlook.com>
2022-11-28 03:03:56 +0000

    more infrastructure

--- src/target_affine_scheme.lean
+++ src/target_affine_scheme.lean
@@ -1,4 +1,8 @@
 import algebraic_geometry.AffineScheme
+
+import random_lemmas
+import about_local_rings
+
 /-
 
 # 01I1
@@ -9,9 +13,51 @@ noncomputable theory
 
 universes u
 
+open opposite
+open Top.presheaf
+open category_theory
+
 namespace algebraic_geometry
 
 variables (X Y : Scheme.{u}) [is_affine Y]
+
+namespace Scheme
+
+instance gloabl_sections_algebra (y : Y.carrier) :
+  algebra (Scheme.Γ.obj $ op Y) $ Y.presheaf.stalk y :=
+ring_hom.to_algebra $ ring_hom.comp 
+  (PresheafedSpace.stalk_map Y.iso_Spec.hom.1 _) $
+  (structure_sheaf.stalk_iso _ _).inv.comp $ 
+  @algebra_map (Γ.obj $ op Y) 
+    (localization.at_prime (Y.iso_Spec.hom.1.base y).as_ideal) _ _ $
+      by { dsimp, exact localization.algebra }
+
+instance stalk_is_localization (y : Y.carrier) : 
+  @@is_localization.at_prime _ (Y.presheaf.stalk y)
+    _ (algebraic_geometry.Scheme.gloabl_sections_algebra Y y) 
+    (Y.iso_Spec.hom.1.base y).as_ideal _ :=
+{ map_units := λ z, 
+  begin 
+    rw [ring_hom.algebra_map_to_algebra, ring_hom.comp_apply, 
+      ring_hom.comp_apply],
+    refine is_unit.map _ (is_unit.map _ _),
+    erw [←localization.mk_algebra_map, algebra.algebra_map_self], 
+    rw [ring_hom.id_apply, localization.at_prime.mk_is_unit_iff],
+    exact z.2,
+  end,
+  surj := λ z,
+  begin 
+    rw [ring_hom.algebra_map_to_algebra],
+    let z' := (stalk_specializes _ _ ≫ PresheafedSpace.stalk_map Y.iso_Spec.inv.1
+      (Y.iso_Spec.hom.1 y)) z,
+    work_on_goal 2
+    { erw [←Scheme.comp_val_base_apply, iso.hom_inv_id, Scheme.id_val_base, 
+        id_apply], },
+    sorry
+  end,
+  eq_iff_exists := sorry }
+
+end Scheme
 
 namespace Scheme.hom
 
